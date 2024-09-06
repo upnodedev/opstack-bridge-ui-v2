0023723297802634702042c85448602ad32d98da
france <frongrider2@gmail.com>
2024-09-06 17:59:36 +0700

    add custom icon and title

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 import path from 'path';
+import { createHtmlPlugin } from 'vite-plugin-html';
+import { loadEnv } from 'vite';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  return {
+    plugins: [
+      react(),
+      createHtmlPlugin({
+        minify: true,
+        inject: {
+          data: {
+            title: env.VITE_APP_NAME,
+            logo: env.VITE_APP_LOGO,
+          },
+        },
+      }),
+    ],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
     },
-  },
-})
+  };
+});
